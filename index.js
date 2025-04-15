792731f621abd9d1ceb537729c2179c343d5d99b
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2025-04-15 17:07:57 +0200

    add expression affixion service

--- index.js
+++ index.js
@@ -11,20 +11,27 @@ app.use(bodyParser.urlencoded({ extended : true}));
 const API_URL = "https://newton.now.sh/api/v2/";
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { operation: null });
 });
 
-app.get("/", async (req, res) => {
+app.post("/score", async (req, res) => {
   const selectedOperation = req.body.operation;
+  const expression = req.body.expression;
+
+  if (!expression || expression.trim() === "") {
+    return res.render("index.ejs", {
+      operation: { error: "Expression field can not be empty!" }
+    });
+  }
   try{
-    const result = await axios.get(API_URL + selectedOperation);
-    res.render("index.ejs", {operation : JSON.stringify(result.data)})
+    const encodedExpression = encodeURIComponent(expression); 
+    const response = await axios.get(`${API_URL}${selectedOperation.toLowerCase()}/${encodedExpression}`);
+    res.render("index.ejs", {operation : response.data})
   } catch(error) {
-    console.log(error.result.data);
+    console.log(error.response.data);
     res.status(500);
   }
-})
-
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
